c817f9677c34e7faf7054fd27b2c08dfe6b037d7
york135 <b06902046@ntu.edu.tw>
2020-06-14 23:50:51 +0800

    add efficient-b0 (class Effnetb0)

--- sing_voice_transcription/efficientnet/net/effnet.py
+++ sing_voice_transcription/efficientnet/net/effnet.py
@@ -1,6 +1,31 @@
 import torch.nn as nn
 import torch
 
+class EffNetb0(nn.Module):
+    def __init__(self, output_size=52):
+        super(EffNetb0, self).__init__()
+        self.model_name = 'effnet'
+
+        # Create model
+        torch.hub.list('rwightman/gen-efficientnet-pytorch')
+        self.effnet = torch.hub.load('rwightman/gen-efficientnet-pytorch', 'efficientnet_b0', pretrained=False)
+        
+        self.effnet.conv_stem = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+        # Modify last linear layer
+        num_ftrs = self.effnet.classifier.in_features
+        self.effnet.classifier= nn.Linear(num_ftrs, output_size)
+        # print (self.effnet)
+        
+    def forward(self, x):
+        out = self.effnet(x)
+        # print(out.shape)
+        # [batch, output_size]
+
+        onset_logits = out[:, 0]
+        offset_logits = out[:, 1]
+        pitch_logits = out[:, 2:]
+
+        return onset_logits, offset_logits, pitch_logits
 
 class EffNet(nn.Module):
     def __init__(self, output_size=52):
@@ -16,13 +41,6 @@ class EffNet(nn.Module):
         num_ftrs = self.effnet.classifier.in_features
         self.effnet.classifier= nn.Linear(num_ftrs, output_size)
         #print (self.effnet)
-        
-        """
-
-        # Modify first conv layer
-        num_fout = self.resnet.conv1.out_channels
-        self.resnet.conv1 = nn.Conv2d(1, num_fout, kernel_size=7, stride=2, padding=3, bias=False)
-        """
 
     def forward(self, x):
         out = self.effnet(x)
@@ -38,5 +56,8 @@ class EffNet(nn.Module):
 
 if __name__ == '__main__':
     from torchsummary import summary
+    model = EffNetb0().cuda()
+    summary(model, input_size=(1, 1795, 7))
+
     model = EffNet().cuda()
     summary(model, input_size=(1, 1795, 7))
